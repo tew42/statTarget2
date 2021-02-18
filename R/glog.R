7ae8d3b267a266ce6501a9b6bf20f835e4e93cc2
tew42 <2049715+tew42@users.noreply.github.com>
2021-02-18 17:11:10 +0100

    Update glog.R

--- R/glog.R
+++ R/glog.R
@@ -1,4 +1,10 @@
 glog <- function(y, lambda) {
-    yt <- log2(y + sqrt(y^2 + lambda))
+    y <- as.matrix(y)
+    if ( typeof(lambda) != "logical" ){
+        yt <- log2( (y-lambda[1] + sqrt((y-lambda[1])^2 + (lambda[2])^2))/2 )
+    } else {
+        lambda <- colQuantiles(abs(y),prob=0.03,type=8)
+        yt <- t(apply(y, 1, function(var) ( log2( (var + sqrt(var^2 + lambda^2) )/2 ) )))
+    }
     return(yt)
 }
